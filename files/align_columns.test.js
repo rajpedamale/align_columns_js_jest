f38d008fc6aba8896a679191f88adeeca2725a63
j0nqm2 <j0nqm2@cyber-dojo.org>
2023-02-10 03:14:31 +0000

    3 ran tests, predicted red, got amber

--- files/align_columns.test.js
+++ files/align_columns.test.js
@@ -2,10 +2,14 @@
 
 const align_columns = require('./align_columns');
 
-const sampleText = '';
+const sampleText = 'Given$a$text$file$of$many$lines,$where$fields$within$a$line$';
 
 describe('Align Columns', () => {
   it('should return text', () => {
     expect(typeof align_columns(sampleText)).toEqual('string');
   });
+  
+  it('should return white space separated text for $ separated text', () => {
+    expect(align_columns(sampleText)).toEqual('Given a text file of many lines, where fields within a line ');
+  }
 });
