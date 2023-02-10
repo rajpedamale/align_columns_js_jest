6e5cdfbfc64eee18910f00a2824273a9fd839aaf
j0nqm2 <j0nqm2@cyber-dojo.org>
2023-02-10 03:19:25 +0000

    6 ran tests, predicted green, got green

--- files/align_columns.test.js
+++ files/align_columns.test.js
@@ -3,6 +3,8 @@
 const align_columns = require('./align_columns');
 
 const sampleText = 'Given$a$text$file$of$many$lines,$where$fields$within$a$line$';
+const sampleTextSingleLine = 'Given$a$text$file$of$many$lines,$where$fields$within$a$line$';
+const alignedTextSingleLine = 'Given a text file of many lines, where fields within a line ';
 
 describe('Align Columns', () => {
   it('should return text', () => {
@@ -10,6 +12,6 @@ describe('Align Columns', () => {
   });
   
   it('should return white space separated text for $ separated text', () => {
-    expect(align_columns(sampleText)).toEqual('Given a text file of many lines, where fields within a line ');
+    expect(align_columns(sampleTextSingleLine)).toEqual(alignedTextSingleLine);
   });
 });
