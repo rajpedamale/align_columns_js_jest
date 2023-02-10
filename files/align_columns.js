e497fc07419cd0960fda40c4a96c528eb72f4d89
j0nqm2 <j0nqm2@cyber-dojo.org>
2023-02-10 03:36:10 +0000

    10 ran tests, predicted green, got green

--- files/align_columns.js
+++ files/align_columns.js
@@ -2,7 +2,7 @@
 
 function align_columns(delimitedText) {
   const lines = delimitedText.split('\n').map(line => line.split('$'));
-  return lines.map((line => line.join(' '))).join(' ');
+  return lines.map((line => line.join(' '))).join('\n');
 }
 
 module.exports = align_columns;
