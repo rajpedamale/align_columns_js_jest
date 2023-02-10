f375be9c171f604e3a37a8151fa2706cd1f7ac2d
j0nqm2 <j0nqm2@cyber-dojo.org>
2023-02-10 05:23:16 +0000

    19 ran tests, predicted green, got green

--- files/align_columns.test.js
+++ files/align_columns.test.js
@@ -10,7 +10,7 @@ const alignedSampleText =
 `Given  a          text      file   of     many     lines,     where    fields within  a  line
 are    delineated by        a      single 'dollar' character, write    a      program
 that   aligns     each      column of     fields   by         ensuring that   words   in each
-column are        separated by     at  least  one      space.`;
+column are        separated by     at     least    one        space.`;
 
 const sampleTextSingleLine = 'Given$a$text$file$of$many$lines,$where$fields$within$a$line$';
 const alignedTextSingleLine = 'Given a text file of many lines, where fields within a line';
